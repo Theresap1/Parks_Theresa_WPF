64d170bd6a84feb47b804210739d6d553aab118b
Theresa Parks <TheresaP@fullsail.edu>
2014-06-19 18:37:05 -0400

    added var and console.log
    I added the variable and console.log codes

--- Parks_Theresa_Expressions_Assignment/Wacky/js/script.js
+++ Parks_Theresa_Expressions_Assignment/Wacky/js/script.js
@@ -9,4 +9,14 @@
 //create a js file that will tell people how they can save and how long it would take
 // to save the amount they wish to save.
 
+//create a variable to hold persons name
+var name = prompt( " Please type in your name:");
+
+//console.log out that name
+console.log(name);
+
+//Alert user and tell them Welcome
+alert(" Welcome" +name+"! Do you know how much you can save with little effort?");
+
+alert(" Welcome" +name+"! I call this my money machine.");
 
