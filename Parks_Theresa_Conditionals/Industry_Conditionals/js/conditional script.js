b84aa3f36557b260c4dcbcaf22d8f1a4398cf990
Theresa Parks <TheresaP@fullsail.edu>
2014-06-21 00:13:52 -0400

    made var for my tool box
    made var for my tool box

--- Parks_Theresa_Conditionals/Industry_Conditionals/js/conditional script.js
+++ Parks_Theresa_Conditionals/Industry_Conditionals/js/conditional script.js
@@ -6,3 +6,8 @@
  */
 
 //alert ("Testing 1,2,3");
+
+var toolBoxOfTools=("wrench" "pliers" "hammers" "screwdrivers")
+console.log(toolBoxOfTools);
+
+//console.log(toolBoxOfTools[1]);
--- Parks_Theresa_Conditionals/Personal_Conditionals/js/conditional script.js
+++ Parks_Theresa_Conditionals/Personal_Conditionals/js/conditional script.js
@@ -8,10 +8,21 @@
 //alert ("Testing 1,2,3");
 alert ("Testing 1,2,3");
 
-//if temperature in the house is >80 turn on air conditioner.
-//if temperature in the house is <80 turn on fans.
+if temperature in the house is >80 turn on air conditioner.
+if temperature in the house is <80 turn on fans.
 
-if("temp >80"){
-    (" Turn On Air Conditioner");
+if(" temp >80"){
+    (" Turn On Air Conditioner.");{
+}else{/if("temp <80")( " turn on fans.");}
+var prompt;(" Please type in current temperature:");
+
+console.log(" Please type in current temperature:");
+
+var minTemp = 75;
+var highestTemp = 80;
+
+if(minTemp < highestTemp){
+    console.log("you can turn on air conditioner!")
 }else{
- if("temp <80"){ "turn on fans");
+     console.log("you can turn on fans")
+}
