42aaba9f63055c7dd14f93b5af52c42ca65b7e68
Theresa Parks <TheresaP@fullsail.edu>
2014-06-26 22:36:48 -0400

    made var and console.log changes
    made var and console.log changes to get calculation of eleven numbers 1
    to 20

--- Parks_Theresa_Functions/industry/js/function.js
+++ Parks_Theresa_Functions/industry/js/function.js
@@ -6,16 +6,21 @@
 //alert ("Testing 1,2,3");
 
 
-var number = prompt(" Please type in even number between 1 and 20:");
+//var number = prompt(" Please type in even number between 1 and 20:");
 
 
 
 //use a conditional to type an even number between 1 and 20
 
-while (number <= 20){
 
-    out.print(number + " ");
+while (number <= 20)
+{
+    console.log( printnNumber + " evenNumber ");
     number += 2;
 }
 
+if(number != "2,4,6,8,10 "){
+
+ number = prompt("Please type in a even number between 1 and 20 ") ;
+}
 
