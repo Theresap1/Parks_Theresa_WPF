3594bae005ca5070193218b980678032e4a042b5
Theresa Parks <TheresaP@fullsail.edu>
2014-06-26 22:17:02 -0400

    created even number calculation
    created even number calculation

--- Parks_Theresa_Functions/industry/js/function.js
+++ Parks_Theresa_Functions/industry/js/function.js
@@ -3,5 +3,19 @@
  WPF 1406 section 01
  personal
  */
-
 //alert ("Testing 1,2,3");
+
+
+var number = prompt(" Please type in even number between 1 and 20:");
+
+
+
+//use a conditional to type an even number between 1 and 20
+
+while (number <= 20){
+
+    out.print(number + " ");
+    number += 2;
+}
+
+
