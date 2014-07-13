482b9bce254b7f1879fd929fd6da17d30188fc95
Theresa Parks <TheresaP@fullsail.edu>
2014-07-13 12:25:39 -0400

    video on Arrays
    followed with video on how to do an Array

--- ASSOCIATING.JS/js/script.js
+++ ASSOCIATING.JS/js/script.js
@@ -1,13 +1,22 @@
 /**
  * Created by theresaparks on 7/13/14.
  */
-alert("Hello World");
+//alert("Hello World");
 
 
-var age = 48; //how to use a number variable
+//var age = 48; //how to use a number variable
 
-var isStudent = false; // a Boolean variable
+//var isStudent = false; // a Boolean variable
 
-var firstName = "Theresa"; //camelCase
+//var firstName = "Theresa"; //camelCase
 
-var phase = 'I don\'t know \ You\'ll have to speak to the professor'
+//var phase = 'I don\'t know \ You\'ll have to speak to the professor'
+
+//Arrays
+//var friendNames = ["Joe", "John", "james", "Sue"]; //o,1,2,3
+//console.log(friendsNames[o]);
+
+var friendNames =["John", "Sue"];
+
+
+console.log(friendNames[1])
