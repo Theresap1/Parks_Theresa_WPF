a041bd3a5f23d69e383219a7611be67ea45bc516
Theresa Parks <TheresaP@fullsail.edu>
2014-07-13 11:27:57 -0400

    Followed along with videos
    Followed along with videos for alert,using camel casing, boolean
    variable and numbers

--- ASSOCIATING.JS/js/script.js
+++ ASSOCIATING.JS/js/script.js
@@ -4,3 +4,10 @@
 alert("Hello World");
 
 
+var age = 48; //how to use a number variable
+
+var isStudent = false; // a Boolean variable
+
+var firstName = "Theresa"; //camelCase
+
+var phase = 'I don\'t know \ You\'ll have to speak to the professor'
