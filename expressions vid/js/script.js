2873b4ff69230b09b16b6eb56674f256a5709db2
Theresa Parks <TheresaP@fullsail.edu>
2014-07-13 22:25:20 -0400

    Expressions cancatenating strings
    Expressions cancatenating strings

--- expressions vid/js/script.js
+++ expressions vid/js/script.js
@@ -3,7 +3,16 @@
  */
 //Expressions
 
-var a = 2; //sets up our variable a and defines it with 2
-var b;
-b = a +3; //add 3 to a
-console.log(a);
+//var a = 2; //sets up our variable a and defines it with 2
+//var b;
+//b = a +3; //add 3 to a
+//console.log(a);
+
+//Expressions concatenating strings
+
+var firstName = "Theresa";
+var lastName = "Parks";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
