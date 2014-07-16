baad7eaef75cedf27a2e2d61028ac5a6d9af4dd6
Theresa Parks <TheresaP@fullsail.edu>
2014-07-15 22:57:50 -0400

    completed the personal calculation
    completed the personal calculation on packs of cigarettes smoked

--- Expressions assignments/Personal/JS/script.js
+++ Expressions assignments/Personal/JS/script.js
@@ -1,3 +1,19 @@
 /**
  * Created by theresaparks on 7/15/14.
  */
+//alert ("testing 1,2,3");
+
+//create variable for how many cigarettes smoked per day
+var cigsSmoked =prompt("how many packs cigarettes you smoke a day");
+console.log(cigsSmoked);
+
+//create a variable for how many days they smoke
+var numberOfDays = prompt("how many days do you smoke?");
+console.log(numberOfDays);
+
+//calculations of cigarettes smoked
+var totalCigs =cigsSmoked*numberOfDays;
+console.log(totalCigs);
+
+
+alert(" you smoked "+totalCigs);
