6a86ecd0249e1543c92e4245256500186fe4d1d0
Theresa Parks <TheresaP@fullsail.edu>
2014-07-16 22:55:54 -0400

    put in first prompt an variable
    put in first prompt an variable showing how many crabs per tray

--- Expressions assignments/Industry-specific/js/script.js
+++ Expressions assignments/Industry-specific/js/script.js
@@ -2,6 +2,9 @@
  * Created by theresaparks on 7/15/14.
  */
 
-alert ("testing 1,2,3");
+//alert ("testing 1,2,3");
 
+//create a variable to show how many soft crabs we process in a day
+
+var crabsProcessed =prompt("How many crabs per tray");
 
