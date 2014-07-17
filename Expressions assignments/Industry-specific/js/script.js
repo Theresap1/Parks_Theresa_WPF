856a4d00ee43ec0b217712efa19b8a0eea4584d5
Theresa Parks <TheresaP@fullsail.edu>
2014-07-17 00:12:00 -0400

    made changes to calculations
    made changes to calculations still have NaN error

--- Expressions assignments/Industry-specific/js/script.js
+++ Expressions assignments/Industry-specific/js/script.js
@@ -6,17 +6,17 @@
 
 //create a variable to show how many soft crabs we process in a day
  // create variable for how many trays per case
-var traysPerCase =("3 trays per case");
+var traysPerCase =("trays 3 per case");
 console.log(traysPerCase);
 
 // create variable for how many crabs per tray
-var crabsPerTray =("30 crabs per tray");
+var crabsPerTray =("crabs 30 per tray");
 console.log(crabsPerTray);
 
 
 //create variable to show how many cases per pallet
 
-var casesPerPallet =("24 cases per pallet");
+var casesPerPallet =("cases 24 per pallet");
 console.log(casesPerPallet);
 
 //create variable on how many pallets processed for that day
@@ -26,9 +26,9 @@ console.log(palletsProcessed);
 
 //calculations of total crabs processed for that day
 
-var totalCrabs =traysPerCase*crabsPerTray*casesPerPallet*palletsProcessed;
+var totalCrabs =traysPerCase*crabsPerTray*casesPerPallet;
 console.log(totalCrabs);
 
-
+alert("You Processed this many crabs today"+totalCrabs);
 
 
