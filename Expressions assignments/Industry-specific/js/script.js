6080cdac17e64fc5599b3e40aa60c6f6decfcd3d
Theresa Parks <TheresaP@fullsail.edu>
2014-07-16 23:44:55 -0400

    added prompt user variable
    added variable to prompt user for how many pallets of crabs processed
    for that day and made corrections on other variables

--- Expressions assignments/Industry-specific/js/script.js
+++ Expressions assignments/Industry-specific/js/script.js
@@ -6,20 +6,23 @@
 
 //create a variable to show how many soft crabs we process in a day
  // create variable for how many trays per case
-var crabsProcessed =("3 trays");
+var crabsProcessed =("3 trays per case");
 console.log(crabsProcessed);
 
 // create variable for how many crabs per tray
-var crabsPerTray =("30 crabs");
+var crabsPerTray =("30 crabs per tray");
 console.log(crabsPerTray);
 
 
 //create variable to show how many cases per pallet
 
 var casesPerPallet =("24 cases per pallet");
-console.log("casesPerPallet");
+console.log(casesPerPallet);
 
-//
+//create variable on how many pallets processed for that day
+
+var palletsProcessed =prompt("How many pallets processed today");
+console.log(palletsProcessed);
 
 
 
