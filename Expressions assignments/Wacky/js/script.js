4d796fb5135cc16daf03dd0985f82d13b719f408
Theresa Parks <TheresaP@fullsail.edu>
2014-07-16 22:12:15 -0400

    I did wacky expressions assignment
    I did wacky expressions assignment on how much money you spend in gas.

--- Expressions assignments/Wacky/js/script.js
+++ Expressions assignments/Wacky/js/script.js
@@ -1,3 +1,32 @@
 /**
  * Created by theresaparks on 7/15/14.
  */
+
+
+//alert ("testing 1,2,3");
+
+//how much money you spend in gas per yr.
+
+// create variable for size of gas tank
+var tankSize =prompt("what is the size of your gas tank");
+console.log(tankSize);
+
+//create variable for price of gas
+
+var priceGas =prompt("what is price of gas");
+console.log(priceGas);
+
+
+//create variable how often you fill up car by weeks
+
+var weeksFilled =prompt("how many weeks you fill up vehicle");
+console.log(weeksFilled);
+
+//calculations of tankSize * priceGas * weeksFilled
+
+var totalSpent =tankSize*priceGas*weeksFilled;
+console.log(totalSpent);
+
+alert(" You Spend this much in gas "+totalSpent);
+
+
