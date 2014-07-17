6f59bc09c9b6677d086444f3bc66f23ed491dff0
Theresa Parks <TheresaP@fullsail.edu>
2014-07-16 23:16:14 -0400

    created 2 variables
    created variables to show how many crabs per tray and trays per case

--- Expressions assignments/Industry-specific/js/script.js
+++ Expressions assignments/Industry-specific/js/script.js
@@ -5,6 +5,17 @@
 //alert ("testing 1,2,3");
 
 //create a variable to show how many soft crabs we process in a day
+ // create variable for how many trays per case
+var crabsProcessed =("3 trays");
+console.log(crabsProcessed);
 
-var crabsProcessed =prompt("How many crabs per tray");
+// create variable for how many crabs per tray
+var crabsPerTray =("30 crabs");
+console.log(crabsPerTray);
+
+
+
+
+
+
 
