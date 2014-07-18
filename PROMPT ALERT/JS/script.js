6b9eac60155470f585773d95d30c91b25ceee09d
Theresa Parks <TheresaP@fullsail.edu>
2014-07-17 23:30:29 -0400

    created var for height
    created var for height

--- PROMPT ALERT/JS/script.js
+++ PROMPT ALERT/JS/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by theresaparks on 7/17/14.
  */
+
+//Prompt and Alert
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter width");
+var height = prompt("Please enter the height.");
