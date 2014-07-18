60e717426f27b7ec03d6ca8f4935f10139188705
Theresa Parks <TheresaP@fullsail.edu>
2014-07-17 21:28:03 -0400

    made variables for industry expressions assignment
    made variables for industry expressions assignment and console.log and
    checked console to see if it was working properly.

--- Expressions assignments/Industry-specific/js/script.js
+++ Expressions assignments/Industry-specific/js/script.js
@@ -2,26 +2,30 @@
  * Created by theresaparks on 7/15/14.
  */
 
-alert ("testing 1,2,3");
+//alert ("testing 1,2,3");
 
 
- // create variable for how many trays per case
+// create variable for how many trays per case
 
-
+var traysPerCase=("3 Trays per case");
+console.log(traysPerCase);
 
 
 // create variable for how many crabs per tray
-
+var crabsPerTray=("30 Crabs per tray");
+console.log(crabsPerTray);
 
 
 
 //create variable to show how many cases per pallet
-
+var casesPerPallet=("20 Cases per pallet");
+console.log(casesPerPallet);
 
 
 //create variable on how many pallets processed for that day
 
-
+var palletsProcessed =prompt("How many pallets processed today");
+console.log(palletsProcessed);
 //calculations of total crabs processed for that day
 
 
