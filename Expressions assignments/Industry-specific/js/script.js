705e1ba4830f072a686436f32811b3f8b0409b9a
Theresa Parks <TheresaP@fullsail.edu>
2014-07-17 21:06:08 -0400

    deleted everything in industry folder
    deleted everything in industry folder and started again couldn’t get it
    working

--- Expressions assignments/Industry-specific/js/script.js
+++ Expressions assignments/Industry-specific/js/script.js
@@ -2,35 +2,32 @@
  * Created by theresaparks on 7/15/14.
  */
 
-//alert ("testing 1,2,3");
+alert ("testing 1,2,3");
 
-//create a variable to show how many soft crabs we process in a day
+
  // create variable for how many trays per case
-var traysPerCase =("trays 3 per case");
-console.log(traysPerCase);
+
+
+
 
 // create variable for how many crabs per tray
-var crabsPerTray =("crabs 30 per tray");
-console.log(crabsPerTray);
+
+
 
 
 //create variable to show how many cases per pallet
 
-var casesPerPallet =("cases 24 per pallet");
-console.log(casesPerPallet);
+
 
 //create variable on how many pallets processed for that day
 
-var palletsProcessed =prompt("How many pallets processed today");
-console.log(palletsProcessed);
 
 //calculations of total crabs processed for that day
 
-var totalCrabsPerPallet =traysPerCase*crabsPerTray*casesPerPallet;
-console.log(totalCrabsPerPallet);
 
-var totalCrabsPocessed =totalCrabsPerPallet*totalPallets;
 
-alert("You Processed this many crabs today"+totalCrabsPerPallet);
+
+
+alert(" You Processed this many crabs today " +totalCrabsPocessed);
 
 
