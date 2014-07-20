2c4029fcd97ab80041dde9ed2aec36b9357a4263
Theresa Parks <TheresaP@fullsail.edu>
2014-07-20 14:02:13 -0400

    created conditional folder
    created conditional folder and created Industry folder and created
    conditional statement deciding if can take off on saturday depending on
    hours worked

---
+++ Conditionals Assignments/Industry/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Created by theresaparks on 7/20/14.
+ */
+
+
+//alert ("testing 1,2,3");
+
+//prompt user to how many hours worked by friday
+
+var name =prompt("Please type in your name");
+console.log(name);
+
+alert (" Hello "  +name);
+
+prompt("Do you want Saturaday off");
+
+
+var hoursWorked = prompt("Hours worked by friday");
+console.log(hoursWorked);
+
+
+saturday = (hoursWorked < 40) ? " working on saturday" : "off saturday ";
+console.log(saturday);
+
+alert ("you are " + saturday);
