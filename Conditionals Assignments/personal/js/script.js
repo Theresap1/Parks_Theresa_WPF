515957d7fb29005538d367273af13977d4634323
Theresa Parks <TheresaP@fullsail.edu>
2014-07-21 22:05:31 -0400

    made few changes to conditional statement
    made few changes to conditional statement still not working properly

--- Conditionals Assignments/personal/js/script.js
+++ Conditionals Assignments/personal/js/script.js
@@ -5,18 +5,23 @@
 //alert (" testing 1,2,3 ");
 
 var name =prompt("What is your name");
-console.log(name);
-var amountSaved=prompt("How much money have you saved.500,1000,3000");
-console.log("amountSaved");
+    console.log(name);
+
+var amountSaved=prompt("How much money have you saved 500 or 1000 or 3000");
+    console.log(amountSaved);
 
 
-if(amountSaved == 500){
-    console.log("you go to atlantic City");
+if  (amountSaved == 500){
+    console.log ("you go to atlantic City");
 }
 
-else if(amountSaved == 1000){
-    console.log(" you go to Vegas");
+else if (amountSaved == 1000){
+    console.log (" you go to Vegas");
 }
-else (amountSaved == 5000){
-    console.log("you go to disney world");
-}
+
+else  (amountSaved == 3000){
+    console.log (" you go to Disney world");
+
+};
+
+
