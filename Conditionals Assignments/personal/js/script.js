ab94c15af8536eebf3bf5c84a49fc2f05ba6d4c4
Theresa Parks <TheresaP@fullsail.edu>
2014-07-21 21:19:22 -0400

    made an if, if else, else conditional statement
    made an if, if else, else conditional statement still needs debugging

---
+++ Conditionals Assignments/personal/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by theresaparks on 7/20/14.
+ */
+
+//alert (" testing 1,2,3 ");
+
+var name =prompt("What is your name");
+console.log(name);
+var amountSaved=prompt("How much money have you saved.500,1000,3000");
+console.log("amountSaved");
+
+
+if(amountSaved == 500){
+    console.log("you go to atlantic City");
+}
+
+else if(amountSaved == 1000){
+    console.log(" you go to Vegas");
+}
+else (amountSaved == 5000){
+    console.log("you go to disney world");
+}
