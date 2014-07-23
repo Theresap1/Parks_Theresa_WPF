ad5d00f3b16e8144e852e5e5fddabfd078b8fab7
Theresa Parks <TheresaP@fullsail.edu>
2014-07-22 20:05:27 -0400

    finished changes for industry conditional
    finished changes for industry conditional statement and is now working

--- Conditionals Assignments/personal/js/script.js
+++ Conditionals Assignments/personal/js/script.js
@@ -7,22 +7,18 @@
 var name =prompt("What is your name");
     console.log(name);
 
-var amountSaved=prompt("How much money have you saved 500 or 1000 or 3000");
+var amountSaved=Number(prompt("How much money have you saved 500 or 1000 or 3000"));
     console.log(amountSaved);
 
 
-if  (amountSaved == 500){
-
-    console.log("you go to atlantic City");
-
-
-}else if{ (amountSaved == 1000)
-
-    console.log(" you go to Vegas");
-}else  (amountSaved == 3000){
-
-    console.log(" you go to Disney world");}
+if (amountSaved===3000){
+    console.log("goToAtlanticCity");
+} else if(amountSaved===1000 ){
+    console.log("go to vegas");
 
 
-
-
+}else if(amountSaved === 500){
+  console.log("go to disney");
+}else{
+    console.log("You need to type in 500, 1000, 3000. ")
+}
