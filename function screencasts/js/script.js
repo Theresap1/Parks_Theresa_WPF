f7a23e429992b9fafae16f82127ec200d6b0963f
Theresa Parks <TheresaP@fullsail.edu>
2014-07-28 00:07:59 -0400

    scoped out the var
    scoped out the var inside and outside the function

--- function screencasts/js/script.js
+++ function screencasts/js/script.js
@@ -14,9 +14,13 @@
 //functions have names so we can refer to them later.
 
 
-function outptMsg(){
-    console.log("Good Morning World");
-}
+//function outptMsg(){
+    //console.log("Good Morning World");
+//}
+
+
+
+var width = 5;
 
 function calcArea(){
     var width = 28;
@@ -29,3 +33,11 @@ function calcArea(){
 calcArea();
 calcArea();
 calcArea();
+
+// variable scope
+// inside and outside of functions
+
+console.log(width);
+
+//if it is called outside the function it will use the var that is outside
+//if it is called on the inside of the function then it will use the var in the function
