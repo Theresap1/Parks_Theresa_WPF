cec13d75f4abea61147a660d8c76610c02082e75
Theresa Parks <TheresaP@fullsail.edu>
2014-07-28 00:40:57 -0400

    made function for dogYears
    made function for dogYears with arguments and parameters

--- function screencasts/js/script.js
+++ function screencasts/js/script.js
@@ -20,7 +20,7 @@
 
 
 
-var width = 5;
+/*var width = 5;
 
 function calcArea(){
     var width = 28;
@@ -41,3 +41,51 @@ console.log(width);
 
 //if it is called outside the function it will use the var that is outside
 //if it is called on the inside of the function then it will use the var in the function
+*/
+
+/*Arguments and Parameters
+
+ */
+
+
+/*functionName (argument1, argument2);
+
+function funcName (parameter1, parameter2){
+    //code the function runs
+}
+calcArea(30, 20);//arguments
+// inside the () need to becareful of the order they are put in
+
+function calcArea(w,h){ // w=30 h=20    // parameters
+    var area =w * h;
+    console.log(area);
+}
+*/
+function dogYears(age){ //put age in the () to be able to get multiple ansers when called
+    //var age =4;
+    var dogYears = age * 7;
+    console.log("Sparky is "+ dogYears + "years old.")
+}
+//has to be called to work
+dogYears(age1);
+dogYears(2);
+var age1 = 4;// get same results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
