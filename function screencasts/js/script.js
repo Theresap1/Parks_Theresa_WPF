c381230340efb7bc85046e99a4f99a2065aa64bf
Theresa Parks <TheresaP@fullsail.edu>
2014-07-27 23:49:15 -0400

    created basic structure for function
    created basic structure for function

--- function screencasts/js/script.js
+++ function screencasts/js/script.js
@@ -4,4 +4,23 @@
 
 //alert("1,2,3");
 
+//function functionName() {
+    //code the function runs
+//}
 
+
+// curly braces are for blocks of code
+// parenthesis are for parameters
+//functions have names so we can refer to them later.
+
+
+function outptMsg(){
+    console.log("Good Morning World");
+}
+
+function calcArea(){
+    var width = 28;
+    var height =30;
+    var area = width * height;
+    console.log(area);
+}
