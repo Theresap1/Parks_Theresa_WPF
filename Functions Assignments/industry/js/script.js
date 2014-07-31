31eb85601e31a2f2071270c5a4ab7bd5ef0a1663
Theresa Parks <TheresaP@fullsail.edu>
2014-07-31 18:34:23 -0400

    created variables for calculations on discount
    created variables for calculations  on discount given and printed  to
    the console.log

--- Functions Assignments/industry/js/script.js
+++ Functions Assignments/industry/js/script.js
@@ -2,7 +2,7 @@
  * Created by theresaparks on 7/28/14.
  */
 
-alert("1,2,3");
+//alert("1,2,3");
 
 
 //create alert and prompt for users price for house before discount
@@ -12,10 +12,26 @@ alert=prompt(" Type in house price before discount");
 //create a function for first time buyer discount
 
 function firstTimeBuyerDiscount(){
-    
-}
+
+   //create a variable for price of house
+    var housePrice = alert;
+    //create variable for the discount rate 0.25  /  25%
+    var discountRate = 0.25; // = 25%
+    //create variable for discounted amount for price of house
+    var discountAmount =housePrice * discountRate;
+    //create variable for price of house after discount net price
+    var netPrice = housePrice - discountAmount
+    //create variables for the calculations
+    var line1 = " house Price:" + housePrice;
+    var line2 = "discount price:" + discountAmount;
+    var line3 = "net price:" + netPrice;
 
 
+    console.log (line1) + document. write (line1);
+    console.log (line2) + document. write (line2);
+    console.log (line3) + document. write (line3);
+};
 
 
+firstTimeBuyerDiscount();
 
