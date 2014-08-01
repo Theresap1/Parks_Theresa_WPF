e6ee4ea325326f60dd155c701d6fe0c142f2c2bf
Theresa Parks <TheresaP@fullsail.edu>
2014-07-31 20:36:44 -0400

    debugging code
    debugging code still not working

--- Functions Assignments/personal/js/script.js
+++ Functions Assignments/personal/js/script.js
@@ -2,11 +2,15 @@
  * Created by theresaparks on 7/31/14.
  */
 
-alert("a,b,c");
-
+//alert("a,b,c");
+lotterynumber(minNum,maxNum);
 //create name for function for picking lottery numbers
-function lotterynumber(minNum, maxNum){
+function lotteryNumber(){
    // create a variable to hold lottery numbers
-    var lotteryNumbers= Math.random()* (maxNum-minNum)+ minNum;
+   // create a variable to hold the min and max number
+    var min=0,max=96;
+    var lotteryNumber= Math.round(math.random)* (maxNum-minNum)+ Number(minNum);
+    console.log(lotteryNumber);
+};
 
-}
+    //function(lotteryNumber);
