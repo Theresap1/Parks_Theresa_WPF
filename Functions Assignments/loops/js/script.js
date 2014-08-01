732cde28fa10265a8318bd53bef112dec5804522
Theresa Parks <TheresaP@fullsail.edu>
2014-07-31 23:27:51 -0400

    created while loop
    created while loop and write them to the console

--- Functions Assignments/loops/js/script.js
+++ Functions Assignments/loops/js/script.js
@@ -15,6 +15,12 @@ function loop(){
     //create a variable to count 30
 
     var count = 30;
+   // create a while loop to start count from 0
+    while (count>0){
+        Document.write ("the value of counts"+ count+ "<br />");
+        count--;
+
+    }
 
 
 }
